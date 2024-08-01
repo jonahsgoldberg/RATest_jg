38522ca2571bea120803197067050283a139c2e5
Jonah Samuel Goldberg <62079513+jonahsgoldberg@users.noreply.github.com>
2024-08-01 10:36:09 -0400

    Update RDperm.R

--- R/RDperm.R
+++ R/RDperm.R
@@ -225,7 +225,7 @@ calc_stat.CvM<-function(x){
 aqrot<-function(w,W_z){
   w<-W_z[,w]
   z<-W_z[,"z"]
-  N<-length(w)
+  N<-nrow(w)
   t <- seq(from=min(z),to=max(z),length.out = 2*N)
   f <- quantreg::akj(z,t)$dens
   t0 <- which(abs(t-0)==min(abs(t-0)))
@@ -245,7 +245,7 @@ aqrot<-function(w,W_z){
 qrot<-function(w,W_z){
   w<-W_z[,w]
   z<-W_z[,"z"]
-  N<-length(w)
+  N<-nrow(w)
   t <- seq(from=min(z),to=max(z),length.out = 2*N)
   f <- quantreg::akj(z,t)$dens
   t0 <- which(abs(t-0)==min(abs(t-0)))
