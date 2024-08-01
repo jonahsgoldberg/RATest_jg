83345184f7ba674a5b8f92f8321b3c3b11129adb
Jonah Samuel Goldberg <62079513+jonahsgoldberg@users.noreply.github.com>
2024-08-01 12:34:49 -0400

    Update RDperm.R

--- R/RDperm.R
+++ R/RDperm.R
@@ -210,7 +210,7 @@ calc_stat.CvM<-function(x){
   else {
     stat<-apply(x,2,CvM.stat)
     n.stat<-names(stat)
-    K<-dim(x)[2]
+    K<-1
     c<-C.unitsphere(K)
     cS<-as.matrix(x)%*%c
     TSn.joint<-max(apply(cS,2,calc_stat.CvM))
